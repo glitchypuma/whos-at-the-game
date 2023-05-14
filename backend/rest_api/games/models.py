cb74537f2c8f3c26c47978dfecfa08af2a230a5f
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-05-13 18:03:50 -0700

    Create BaseballGame model

--- backend/rest_api/games/models.py
+++ backend/rest_api/games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BaseballGame(models.Model):
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    start_time = models.DateTimeField(
+    db_comment="Date and time baseball game is slated to start.",
+    )
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        #return the task title
+        return self.title

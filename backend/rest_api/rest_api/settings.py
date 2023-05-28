be4ffef5b2f6587aaa77f8129394023d1f99f595
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-05-27 23:27:52 -0700

    Hide api key and update football model
    Updates football time_start field to be CharField, like all other sports

--- backend/rest_api/rest_api/settings.py
+++ backend/rest_api/rest_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# API KEYS
+
+RAPID_API_KEY = os.environ.get('X_RAPIDAPI_KEY')

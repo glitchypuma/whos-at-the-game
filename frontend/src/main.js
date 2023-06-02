eb0eb13c897983b19d5a973f1c8ecd5b2c3c49af
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-06-02 16:37:23 -0700

    Remove commented-out code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,21 +3,6 @@ import axios from 'axios';
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-//import { createApp } from 'vue'
-//import VueRouter from 'vue-router';
-
-
-// axios.defaults.baseURL = 'http://localhost:8080/'
-// const routes = [
-//     {
-//     name: 'Home',
-//     path: '/',
-//     component: App
-//     }
-// ];
-
-// const router = new VueRouter({ mode: 'history', routes: routes })
-// const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = Vue.createApp(App)
 
@@ -26,8 +11,6 @@ axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 
 app
-    //.use(axios)
-    //.use(bodyParser.json())
     .use(VueAxios, axios)
     .use(cors)
     .mount('#app')

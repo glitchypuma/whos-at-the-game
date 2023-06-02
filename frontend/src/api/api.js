5716b593595d207ab9a82a24fa5054c72fbf2c22
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-06-01 18:33:50 -0700

    Let frontend request backend (#13)

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,4 @@
 import dev from './test';
 import prod from './prod';
 
-// MOCK DATA
-// export default process.env.NODE_ENV === 'production' ? prod : dev;
-
-// PROD DATA
-export default process.env.NODE_ENV === 'development' ? prod : dev;
+export default process.env.NODE_ENV === 'production' ? prod : dev;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,26 +3,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-//import { createApp } from 'vue'
-//import VueRouter from 'vue-router';
-
-
-//axios.defaults.baseURL = 'http://localhost:8080/'
-// const routes = [
-//     {
-//     name: 'Home',
-//     path: '/',
-//     component: App
-//     }
-// ];
-
-// const router = new VueRouter({ mode: 'history', routes: routes })
-
 const app = Vue.createApp(App)
 
 app.config.globalProperties.$http = axios;
 
 app
-    //.use(axios)
     .use(VueAxios, axios)
     .mount('#app')

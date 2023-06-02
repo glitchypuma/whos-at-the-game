a7c7c9109a3911501a403182f3ad8134e2f987ca
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-06-01 21:55:01 -0700

    Reach backend on chrome

--- backend/rest_api/rest_api/settings.py
+++ backend/rest_api/rest_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['54.177.45.150', 'gameapi.buzz', 'localhost']
 CORS_ALLOW_ALL_ORIGINS = True

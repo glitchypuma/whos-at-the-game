846a2fad300ddf495c8e6092bc222af8040fbf66
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2023-06-03 21:03:02 -0700

    List guesses

--- frontend/src/components/GameAttendance.vue
+++ frontend/src/components/GameAttendance.vue
@@ -1,9 +1,12 @@
 <template>
     <body>
         <h2>Judging by the conversation on Twitter, here's our best guess:</h2>
-        <p>Game:<br>{{ selectedGame }}</p>
-
-        {{ bestGuess }}
+        <!-- <p>Game:<br>{{ selectedGame }}</p> -->
+        <ul>
+            <li v-for="person in bestGuess" :key="person.id">
+                {{ person.name }}
+            </li>
+        </ul>
     </body>
 </template>
 
@@ -48,5 +51,20 @@ body {
 p{
     margin: 2rem 3rem;
     text-align: start;
+}
+ul {
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  gap: .5rem;
+  list-style-type: none;
+  padding: 0;
+  column-gap: 1rem;
+  margin: 2rem 25%;
+}
+li {
+    text-align: left;
+  background-color: #F2EFE9;
+  border-radius: .5rem;
 }
 </style>

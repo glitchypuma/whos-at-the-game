6675dc6cc5d369e9179f0a3e1ff656a0ad96dccf
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2023-06-05 12:45:37 -0700

    Edit Scraper API to output entity names

--- backend/rest_api/scraper/nlp.py
+++ backend/rest_api/scraper/nlp.py
@@ -1,14 +1,11 @@
 import spacy
 
-if __name__ == '__main__':
-
-    with open('data/raw/tweets.txt', 'r') as file:
-        data = file.read().replace('\n', '')
+def get_names(string):
 
     nlp = spacy.load("en_core_web_lg")
 
     # Created by processing a string of text with the nlp object, replace Hello Wordl! with tweets string
-    doc = nlp(data)
+    doc = nlp(string)
 
     people = []
     
@@ -16,10 +13,13 @@ if __name__ == '__main__':
     for ent in doc.ents:
         # print(ent.text, ent.label_)
         if ent.label_ == 'PERSON':
-            entity = ent.text + ' ' +  ent.label_ + '\n'
+            # entity = ent.text + ' ' +  ent.label_ + '\n'
+            entity = ent.text
             people.append(entity)
 
-    people_str = ' '.join(str(x) for x in people)
-    file = open("data/processed/ents.txt", "w")
-    file.write(people_str)
-    file.close()
+    # people_str = ' '.join(str(x) for x in people)
+    # file = open("data/processed/ents.txt", "w")
+    # file.write(people_str)
+    # file.close()
+    # return people_str
+    return people
